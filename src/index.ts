83fd84eee1d7ddea8f28350ccb98d2382106199e
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2025-10-03 19:23:24 +0200

    manage exports

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,17 @@ export { mixin } from './utils'
 
 export * from './defineElement'
 
-export * from './hooks'
+export {
+  onConnected,
+  onDisconnected,
+  onShouldUpdate,
+  onWillUpdate,
+  onFirstUpdated,
+  onUpdated,
+  onPerformUpdate,
+  onUpdate,
+} from './hooks'
+
 export * from './reactivity'
 
 export * from './utils/mixin'
