2ec9faf180ddef29d748fb2af9ac0682c852f4dc
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2025-10-05 12:27:49 +0200

    reactive type tests

--- tests/reactivity/reactive.spec.ts
+++ tests/reactivity/reactive.spec.ts
@@ -3,18 +3,33 @@ import { describe, expect, it } from 'vitest'
 
 describe('reactivity', () => {
   it('reactive(obj) should work as expected', () => {
-    const subject = { a: 1, b: useRef(3) }
+    const subject = {
+      num: 1,
+      txt: 'text',
+      refNum: useRef(3),
+      refObj: useRef({ r: 2 }),
+      obj: { c: 4 },
+
+      ld: () => 'ok',
+      fn: function () {},
+    }
     const reactiveSubject = reactive(subject)
 
-    console.log(reactiveSubject)
-
     expect(Object.keys(reactiveSubject)).toMatchObject(Object.keys(subject))
-    expect(isRef(reactiveSubject.a)).toBe(false)
-    expect(isRef(reactiveSubject.b)).toBe(false)
+    expect(isRef(reactiveSubject.num)).toBe(false)
+    expect(isRef(reactiveSubject.refNum)).toBe(false)
 
-    reactiveSubject.b = 3
+    reactiveSubject.refNum = 3
 
-    expect(reactiveSubject.b).toBe(3)
-    expect(subject.b.value).toBe(3)
+    expect(reactiveSubject.num).toBe(1)
+    expect(reactiveSubject.txt).toBe('text')
+    expect(reactiveSubject.obj).toMatchObject({ c: 4 })
+    expect(reactiveSubject.refNum).toBe(3)
+    expect(reactiveSubject.refObj).toMatchObject({ r: 2 })
+
+    expect(reactiveSubject.ld).toBeTypeOf('function')
+    expect(reactiveSubject.fn).toBeTypeOf('function')
+
+    expect(subject.refNum.value).toBe(3)
   })
 })
