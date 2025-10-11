4a9068f93242c8e264f541d81f81b19dd26074bd
Joris Aerts <3611758+JorisAerts@users.noreply.github.com>
2025-10-11 10:56:07 +0200

    home page

--- site/src/pages/home.ts
+++ site/src/pages/home.ts
@@ -5,6 +5,7 @@ import { defineElement } from 'lit-composition'
 import '../components/card'
 import cssHome from './home.scss?inline'
 import { logo } from '../utils/logo'
+import { links } from '../router'
 
 defineElement({
   name: 'lc-home',
@@ -17,8 +18,31 @@ defineElement({
           <h1 class="ma-0" style="font-weight: 100; opacity: .8; letter-spacing: -.4px">
             Lit-composition, a new way of defining Web Components
           </h1>
-          <button class="px-4 py-2 mt-4 mr-2" style="font-weight: bold">Getting Started</button>
-          <button class="px-4 py-2 mt-4 mr-2" style="font-weight: bold">Install</button>
+          <button
+            class="px-4 py-2 mt-4 mr-2"
+            style="
+              box-sizing: border-box;
+              border-radius: 6px;
+              font-weight: bold;
+              background: var(--secondary-color);
+              border: 1px solid var(--secondary-color);
+            "
+            @click="${() => window.location.replace(links.GettingStarted)}"
+          >
+            Getting Started
+          </button>
+          <button
+            class="px-4 py-2 mt-4 mr-2"
+            style="
+              box-sizing: border-box;
+              border-radius: 6px;
+              font-weight: bold;
+              background: transparent;
+              border: 1px solid var(--secondary-color);
+            "
+          >
+            npm i lit-composition
+          </button>
         </div>
         <div class="logo-container" style="margin-right: 100px; align-self: center">
           <div class="logo-bg"></div>
